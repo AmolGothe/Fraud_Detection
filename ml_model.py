ec421007e3e5be53f98b5331ef7e2171c4836edc
AmolGothe <amol.gothe@gmail.com>
2024-02-22 10:47:21 +0530

    changes made to model 2

--- ml_model.py
+++ ml_model.py
@@ -4,7 +4,7 @@ def model1():
     print("adding code for ML model 1")
 
 def model2():
-    print("adding code for ML model 2")
+    print("adding code for ML model 2 - this is the best model")
 
 def model3():
     print("adding code for ML model 3")
